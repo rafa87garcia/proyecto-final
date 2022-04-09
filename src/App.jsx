49137ec8569c31531d78524aed8f395146c47b69
Rafael Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-04-09 19:01:14 +0200

    Actualizar rutas y añadir ruta home

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cart from './components/Cart/Cart';
+import Home from './components/Home/Home';
 import ProductDetail from './components/ProductDetail/ProductDetail';
 import ProductList from './components/ProductList/ProductList';
 import Users from './components/Users/Users';
 
 
+
 function App() {
   return (
-   
+
     <Router>
-        <div className='App'>
-          <Routes>
-            <Route path='cart' element={<Cart />} />
-            <Route path='productlist' element={<ProductList />} />
-            <Route path='productdetail/:id' element={<ProductDetail />} />
-            <Route path='users' element={<Users/>} />
-          </Routes>
-        </div>
-     
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/productlist' element={<ProductList />} />
+          <Route path='/productdetail/:id' element={<ProductDetail />} />
+          <Route path='users' element={<Users />} />
+        </Routes>
+      </div>
+
     </Router>
-   
-    
+
+
   )
 }
 
