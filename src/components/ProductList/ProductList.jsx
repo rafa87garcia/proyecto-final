6801f79ef8c4f36611c2516dbaad2c34b68533f5
Andrea <astamariab@gmail.com>
2022-04-09 14:57:40 +0200

    productlist

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function ProductList() {
-  return (
-    <div>ProductList</div>
+
+    const BASEURL = "https://fakestoreapi.com/products";
+    
+    // const [error, setError] = useState(null);
+    // const [isLoaded, setIsLoaded] = useState(false);
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch(BASEURL)
+        .then ((response) => response.json())
+        .then(
+            (response) => {
+                console.log(response);
+                setProducts(response);
+            }
+        )
+
+    },[])
+   
+    return (
+        <div>
+        {products.map((product) => {
+          return (
+            <div key={product.id}>
+              <h3>{product.title}</h3>
+              <p>{product.price}</p>
+              <img src={product.image} alt=""/>
+            </div>
+          );
+        })}
+      </div>
+    
   )
 }
 
