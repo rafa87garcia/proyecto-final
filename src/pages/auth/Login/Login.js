63ba06d6879ffaca13af24bb9a05e73828a14dfb
Rafa Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-05-22 19:46:04 +0200

    Actualizar formulario login y registro con validaciones

--- src/pages/auth/Login/Login.js
+++ src/pages/auth/Login/Login.js
@@ -1,34 +1,56 @@
 import React from 'react';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as yup from "yup";
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  event.stopPropagation();
-
-  const form = event.target;
-
-  const [email, password] = new FormData(form).values();
-  console.log(email);
-  console.log(password);
+const save = (data) => {
+  console.log(data);
 }
+
 
 const Login = () => {
 
+  const schema = yup.object().shape({
+    email: yup.string()
+      .email('Email incorrecto')
+      .required('Requerido'),
+    password: yup.string()
+      .min(2, 'Desmasiado Corto!')
+      .max(70, 'Demasiado largo!')
+      .required('Requerido'),
+  });
+
   return (
-    <form noValidate onSubmit={handleSubmit}>
-      <input
-        name="email"
-        type="email"
-        placeholder="name@example.com"
-        required
-      />
-      <input
-        name="password"
-        type="password"
-        placeholder="Escribe la contraseña"
-        required
-      />
-      <button type='submit'>Login</button>
-    </form>
+    <>
+      <h1>Login</h1>
+
+      <Formik
+        initialValues={{
+          email: '', password: '',
+        }}
+        validationSchema={schema}
+        onSubmit={save}
+      >
+        {({ handleSubmit, handleChange, errors }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            <Field
+              name="email"
+              id="email"
+              type="email"
+              onChange={handleChange}
+            />
+            <ErrorMessage name="email" component="div" />
+            <Field
+              name="password"
+              id="password"
+              type="password"
+              onChange={handleChange}
+            />
+            <ErrorMessage name="password" component="div" />
+            <button type='submit'>Login</button>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 }
 export default Login;
