61edc723861c9ee856af561812dc84cf7b1095e4
Rafa Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-05-26 21:05:16 +0200

    Cambiar class de className

--- src/pages/product/component/ProductList/ProductList.js
+++ src/pages/product/component/ProductList/ProductList.js
@@ -4,38 +4,36 @@ import "./productList.scss"
 
 function ProductList() {
 
-    const BASEURL = "https://fakestoreapi.com/products";
-    
-    // const [error, setError] = useState(null);
-    // const [isLoaded, setIsLoaded] = useState(false);
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        fetch(BASEURL)
-        .then ((response) => response.json())
-        .then(
-            (response) => {
-                console.log(response);
-                setProducts(response);
-            }
-        )
+  const BASEURL = "https://fakestoreapi.com/products";
 
-    },[])
-   
-    return (
-        <div >
-        {products.map((product) => {
-          return (
-            <div className="product-grid" key={product.id}>
-            
-              <h3 class="product-grid__title">{product.title}</h3>
-              
-              <p class="product-grid__price">{product.price}</p>
-              <img class="product-grid__image"src={product.image} alt=""/>
-            </div>
-          );
-        })}
-      </div>
-    
+  // const [error, setError] = useState(null);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch(BASEURL)
+      .then((response) => response.json())
+      .then(
+        (response) => {
+          console.log(response);
+          setProducts(response);
+        }
+      )
+
+  }, [])
+
+  return (
+    <div >
+      {products.map((product) => {
+        return (
+          <div className="product-grid" key={product.id}>
+            <h3 className="product-grid__title">{product.title}</h3>
+            <p className="product-grid__price">{product.price}</p>
+            <img className="product-grid__image" src={product.image} alt="" />
+          </div>
+        );
+      })}
+    </div>
+
   )
 }
 
