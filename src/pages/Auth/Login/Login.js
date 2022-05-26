3330355d33b55ea2e656f95c98c5faf279673381
Rafa Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-05-26 21:46:34 +0200

    Modificar enlace

--- src/pages/Auth/Login/Login.js
+++ src/pages/Auth/Login/Login.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { ErrorMessage, Field, Formik } from 'formik';
+import { ErrorMessage, Formik } from 'formik';
+import { Link } from 'react-router-dom';
 import * as yup from "yup";
+import { Row, Form, Col, Button } from 'react-bootstrap';
+
 import './_login.scss'
-import {Row, Form, Col, Button} from 'react-bootstrap';
 
 const save = (data) => {
   console.log(data);
 }
-
 
 const Login = () => {
 
@@ -34,55 +35,32 @@ const Login = () => {
       >
         {({ handleSubmit, handleChange, errors }) => (
           <Form noValidate onSubmit={handleSubmit} className='login'>
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridEmail">
-                          
-              <Form.Label>Email</Form.Label>
-              <Form.Control name="email"
-                  type='email' 
-                  onChange={handleChange}
-
-              />
-
-                {/* <Field
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
                   name="email"
-                  id="email"
-                  type="email"
+                  type='email'
                   onChange={handleChange}
-                /> */}
+                />
                 <ErrorMessage name="email" component="div" />
-
-            </Form.Group>
-                
+              </Form.Group>
             </Row>
 
             <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridEmail">
-                          
-              <Form.Label>Password</Form.Label>
-              <Form.Control name="password"
-                  type='password' 
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  name="password"
+                  type='password'
                   onChange={handleChange}
-
-              />
-
-              <ErrorMessage name="password" component="div" />        
-
-            </Form.Group>
-                
+                />
+                <ErrorMessage name="password" component="div" />
+              </Form.Group>
             </Row>
-              {/* <Field 
-                name="password"
-                id="password"
-                type="password"
-                onChange={handleChange}
-              />Password
-              <ErrorMessage name="password" component="div" /> */}
-            
-            <br/>
+
             <Button type='submit' variant='outline-primary' size='lg'>Login</Button>
-            {/* <button type='submit'>Login</button> */}
-            <p class="message">New User? <a href="#">Create an account</a></p>
+            <p class="message">New User? <Link to="/register">Create an account</Link></p>
           </Form>
         )}
       </Formik>
