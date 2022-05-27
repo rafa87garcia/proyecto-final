a05da172fdef99f397dfbd46e5e2a63fdb94fb1c
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2022-05-27 19:39:19 +0200

    order scss

--- src/shared/Orders/Orders.js
+++ src/shared/Orders/Orders.js
@@ -1,50 +1,39 @@
+// import { ReactDOM } from 'react';
 import React from 'react'
-import './_orders.scss';
+import './_Orders.scss';
+
+// if (document.getElementById('Orders')) {
+//   ReactDOM.render(<Orders />, document.getElementById('Orders'));
+// }
 const Orders = () => {
+  
   return (
-    <div className='Order'>
-    
-    <h1 className='title'>My Orders</h1>
-    <ul className='list'>
-      <li>Order 1
-        <ul className='orderDetail'>
-          <li>name:</li>
-          <li>description:</li>
-          <li>image:</li>
-          <li>pvp: </li>
-          <li>category: </li>
+    <div className="Order">
+      <h1 className='title'>My Orders</h1>
+        <ul className='list'>
+          <li className='&__titleLi'>Order:
+            <ul className='orderDetail'>
+              <li className="&__name">name:</li>
+              <li className="&__description">description:</li>
+              <li className="&__pvp">pvp: </li>
+              <li className="&__total">total: </li>
+            </ul>
+          </li>
       </ul>
-      </li>
-      <li>Order 2
-      <ul className='orderDetail'>
-          <li>name:</li>
-          <li>description:</li>
-          <li>image:</li>
-          <li>pvp: </li>
-          <li>category: </li>
-        </ul>
-      </li>
-      <li>Order 3
-      <ul className='orderDetail'>
-          <li>name:</li>
-          <li>description:</li>
-          <li>image:</li>
-          <li>pvp: </li>
-          <li>category: </li>
-        </ul>
-      </li>
-      <li>Order 4
-      <ul className='orderDetail'>
-          <li>name:</li>
-          <li>description:</li>
-          <li>image:</li>
-          <li>pvp: </li>
-          <li>category: </li>
-        </ul>
-      </li>
-    </ul>
     </div>
   )
 }
 
 export default Orders
+
+//   <div >
+//     {products.map((product) => {
+//       return (
+//         <div className="product-grid" key={product.id}>
+//           <h3 className="product-grid__title">{product.title}</h3>
+//           <p className="product-grid__price">{product.price}</p>
+//           <img className="product-grid__image" src={product.image} alt="" />
+//         </div>
+//       );
+//     })}
+//   </div>
