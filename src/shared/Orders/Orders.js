3a7eeb5424466ff1fc57f88cf3bc436b9ddf7cb9
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2022-05-27 12:11:47 +0200

    orders

--- src/shared/Orders/Orders.js
+++ src/shared/Orders/Orders.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Orders = () => {
   return (
-    <div >
+    <div className='Order'>
     
     <h1 className='title'>My Orders</h1>
-    <ul className='orderList'>
+    <ul className='list'>
       <li>Order 1</li>
       <li>Order 2</li>
       <li>Order 3</li>
