98f4e21d614511122608cfb0270ae569c6ba632f
Rafa Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-05-29 12:44:27 +0200

    Ocultar admin sin no eres usuario logeado

--- src/shared/components/NavBar/NavBar.js
+++ src/shared/components/NavBar/NavBar.js
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom";
 import "./_navBar.scss";
-
+import useUser from "../../../hooks/useUser";
 
 const NavBar = () => {
+
+  const { userCurrent } = useUser();
+
+  //if (!userCurrent) return ""
+
+
   return (
     <div className="navBar">
-    <ul>
+      <ul>
         <li><Link to="/">All Products</Link></li>
         <li><Link to="aboutus">About us</Link></li>
-        <li><Link to="admin">Admin</Link></li>
+        {userCurrent && (<li><Link to="admin">Admin</Link></li>)}
         <li><Link to="orders">My orders</Link></li>
         <li><Link to="contact">Contact</Link></li>
-    </ul>
+      </ul>
     </div>
   )
 }
