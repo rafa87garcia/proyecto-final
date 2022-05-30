e95ce4673982089dcdce48cecc7f7927e2adacb3
Rafa Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-05-30 23:30:39 +0200

    Redirigir formularios

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,9 +1,11 @@
 import { useContext, useEffect, useState } from 'react';
 import Context from '../context/StaticContext';
 import { add, login } from '../shared/services/user';
+import { useNavigate } from 'react-router-dom';
 
 const useUser = () => {
 
+  const navigate = useNavigate();
   const [userCurrent, setUserCurrent] = useState();
   const [token, setToken] = useState();
   const { userContext, setUserContext } = useContext(Context);
@@ -22,18 +24,19 @@ const useUser = () => {
     try {
       const { data: { user, token } } = await login(data);
 
-      const tokenStogare = localStorage.setItem("token", token);
-      setTokenContext(tokenStogare);
+      localStorage.setItem("token", token);
+      setTokenContext(token);
       setUserContext(user);
-
+      navigate("/", { replace: true });
     } catch (error) { }
   }
 
   const userRegister = async (data) => {
+
     if (!data) { return; }
     try {
-     await add(data);
-
+      await add(data);
+      navigate("/", { replace: true });
     } catch (error) { }
   }
 
